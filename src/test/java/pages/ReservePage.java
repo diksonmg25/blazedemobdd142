1fb007c7129a3d44505044cbd2ec386242d33ebd
diksonmg25 <diksonaraguari@gmail.com>
2025-05-28 11:34:05 -0300

    Page Objects Versão Final

--- src/test/java/pages/ReservePage.java
+++ src/test/java/pages/ReservePage.java
@@ -19,8 +19,8 @@ public class ReservePage extends CommonPage {
     // cabeçalho dos vôos
 
     public By byOrder(int ordem_do_voo){
-        return By.xpath("//table[@class='table']/tbody/tr[" + ordem_do_voo + "]//input[@type='submit']");
-
+       return By.xpath("//table[@class='table']/tbody/tr[" + ordem_do_voo + "]//input[@type='submit']");
+      
        // tbody/tr[2] - pegar o elemento da segunda linha - numero de voo - 234
        // segunda opção para pegar o elemento na página 
        ///html/body/div[2]/table/tbody/tr[2]/td[1]/input
@@ -31,9 +31,10 @@ public class ReservePage extends CommonPage {
         return flightsHeader.getText(); // retorna o elemento h3 e o texto dele
     }
 
-    public void clicarNoVoo(int ordem_do_voo){
+    public void clicarNoVoo(int ordem_do_voo) {
         driver.findElement(byOrder(ordem_do_voo)).click();
     }
+
 
 }
 
